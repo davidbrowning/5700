fe102896e336633c51e7022a96ece56b8cd853cb
Stephen Clyde <swc@mdsc.com>
2017-09-05 18:33:50 -0600

    Added version 3 of the SimpleClassHierarchy example

---
+++ examples/SimpleClassHierarchy/SimpleClassHierarchy - Version3 - Better Encapsulation and Strategy/MyClasses/JsonImporterExporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace MyClasses
+{
+    public class JsonImporterExporter : ImporterExporter
+    {
+        private static readonly DataContractJsonSerializer JsonSerializer = new DataContractJsonSerializer(typeof(List<ThingABob>),
+                     new [] { typeof(ThingABob), typeof(Gadget), typeof(Widget) });
+
+        public override void Write(List<ThingABob> data, string filename)
+        {
+            filename = AppendExtension(filename, "json");
+            StreamWriter writer = new StreamWriter(filename);
+            JsonSerializer.WriteObject(writer.BaseStream, data);
+            writer.Close();
+        }
+
+        public override void Read(List<ThingABob> list, string filename)
+        {
+            filename = AppendExtension(filename, "json");
+            StreamReader reader = new StreamReader(filename);
+            List<ThingABob> data = JsonSerializer.ReadObject(reader.BaseStream) as List<ThingABob>;
+            if (data!=null)
+            {
+                foreach(ThingABob thing in data)
+                    list.Add(thing);
+            }
+        }
+    }
+}
---
+++ examples/SimpleClassHierarchy/SimpleClassHierarchy - Version3 - Better Encapsulation and Strategy/MyClasses/ThingABob.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyClasses
+{
+    [DataContract]
+    public class ThingABob
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id={Id}, Name={Name}";
+        }
+    }
+}
