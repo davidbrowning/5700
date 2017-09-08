bbdfcfaa1e943a7b323c023abe9cc0419c7fba32
Stephen Clyde <swc@mdsc.com>
2017-09-08 14:10:35 -0600

    Added some more test cases

--- examples/SimpleClassHierarchy/SimpleClassHierarchy - Version4 - UnitTesting/MyClassesTest/GadgetTester.cs
+++ examples/SimpleClassHierarchy/SimpleClassHierarchy - Version4 - UnitTesting/MyClassesTest/GadgetTester.cs
@@ -17,11 +17,35 @@ namespace MyClassesTest
         }
 
         [TestMethod]
-        public void Gadget_ToString()
+        public void Gadget_Typical_ToString()
         {
             Gadget gadget = new Gadget() { Id = 1, Length = 10, Width = 5, Name = "First Gadget" };
 
             Assert.AreEqual("Gadget: Id=1, Name=First Gadget, width=5, length=10, cost=100", gadget.ToString());
+        }
+
+        [TestMethod]
+        public void Gadget_WithNoName_ToString()
+        {
+            Gadget gadget = new Gadget() { Id = 1, Length = 10, Width = 5 };
+
+            Assert.AreEqual("Gadget: Id=1, Name=, width=5, length=10, cost=100", gadget.ToString());
+        }
+
+        [TestMethod]
+        public void Gadget_NoLengthOrWidth_ToString()
+        {
+            Gadget gadget = new Gadget() { Id = 1, Name = "First Gadget" };
+
+            Assert.AreEqual("Gadget: Id=1, Name=First Gadget, width=0, length=0, cost=0", gadget.ToString());
+        }
+
+        [TestMethod]
+        public void Gadget_NoId_ToString()
+        {
+            Gadget gadget = new Gadget() { Length = 10, Width = 5, Name = "First Gadget" };
+
+            Assert.AreEqual("Gadget: Id=0, Name=First Gadget, width=5, length=10, cost=100", gadget.ToString());
         }
     }
 }
