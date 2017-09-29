a0b0ea4e1ce87724d649c56b8b686a95c1b99b3e
Stephen Clyde <swc@mdsc.com>
2017-09-28 19:14:19 -0600

    Added sample Race Monitor system that sets up and run a RaceDataSimulator.

---
+++ hw2/MyRaceMonitor_CSharp/AppLayer/SimulatorController.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Threading;
+using RaceData;
+
+namespace AppLayer
+{
+    public class SimulatorController
+    {
+        private SimulatedDataSource _simluatedData;
+        public void Run(string inputFileName)
+        {
+            IAthleteUpdateHandler handler = new DataProcessor();
+            _simluatedData = new SimulatedDataSource()
+            {
+                InputFilename = inputFileName,
+                Handler = handler
+            };
+
+            _simluatedData.Start();
+
+            // Let the simulator run for 3 minutes
+            Thread.Sleep(180000);
+
+            _simluatedData.Stop();
+        }
+    }
+}
---
+++ hw2/MyRaceMonitor_CSharp/MyRaceMonitor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppLayer;
+
+namespace MyRaceMonitor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            SimulatorController controller = new SimulatorController();
+            controller.Run("../../../SimulationData/Short Race Simulation-01.csv");
+        }
+    }
+}
